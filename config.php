703d908d6896d391959846829f4d38916c813e05
dandeveloper76 <48998607+dandeveloper76@users.noreply.github.com>
2019-10-25 10:11:40 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define( 'BASE_PATH', 'http://172.18.2.166:8080/out/');//Ruta base donde se encuentra
+define( 'BASE_PATH', 'http://172.18.X.XXX:8080/out/');//Ruta base donde se encuentra
 define( 'DB_HOST', 'localhost' );//Servidor de la base de datos
 define( 'DB_USERNAME', 'root');//Usuario de la base de datos
 define( 'DB_PASSWORD', 'tucontraseña');//Contraseña de la base de datos
